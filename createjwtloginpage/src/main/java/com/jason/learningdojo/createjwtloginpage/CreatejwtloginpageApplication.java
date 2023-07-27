e743aa8af6e62e827e714425ec0b5ad75d8c3529
jasonlau <jasonlauxd@gmail.com>
2023-07-27 20:57:48 +0800

    first commit

---
+++ createjwtloginpage/src/main/java/com/jason/learningdojo/createjwtloginpage/CreatejwtloginpageApplication.java
@@ -0,0 +1,13 @@
+package com.jason.learningdojo.createjwtloginpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatejwtloginpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatejwtloginpageApplication.class, args);
+	}
+
+}
---
+++ createjwtloginpage/src/test/java/com/jason/learningdojo/createjwtloginpage/CreatejwtloginpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.learningdojo.createjwtloginpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatejwtloginpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
